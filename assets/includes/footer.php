e101198a67da06ab60c024b26faee78bee63bcc8
KVMSAANDEEPA <malithsandeepa1081@gmail.com>
2025-08-26 12:11:20 +0530

    test php

---
+++ assets/includes/footer.php
@@ -0,0 +1,13 @@
+<!-- ======= Footer ======= -->
+<footer class="text-gray-300 py-12 bg-[#541C1C]/50 border-t border-white/10">
+        <div class="container mx-auto px-6 text-center">
+            <h3 class="text-2xl font-bold mb-4 text-white">Contact Us</h3>
+            <p class="mb-2">Organizing Committee, INSPIRA'25</p>
+            <p class="mb-2">Email: <a href="mailto:info@inspira.com" class="hover:text-orange-400">info@inspira2025.online</a>
+            </p>
+            <p>Phone: <a href="tel:+94727688334" class="hover:text-orange-400">+94 (72) 768-8334</a></p>
+            <div class="mt-6 border-t border-gray-700 pt-6">
+            <p>&copy; inspira'25. All Rights Reserved.</p>
+            <p><small>Developed By <a href="developers.html" style="text-decoration: underline; color: yellow;">Team INSPIRA</a></small></p></div></div>
+        </div>
+    </footer>
