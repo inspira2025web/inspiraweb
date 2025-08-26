3907b41deb8740b85644b1b1996e2ef404159e61
KVMSAANDEEPA <malithsandeepa1081@gmail.com>
2025-08-26 12:16:37 +0530

    Revert "test php"
    This reverts commit e101198a67da06ab60c024b26faee78bee63bcc8.

--- assets/includes/js-links-inc.php
+++
@@ -1,4 +0,0 @@
-<!-- about page JS -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
