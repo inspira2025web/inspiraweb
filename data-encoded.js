7f9e7dc062986035232ffc6b47734db0e518c895
Propix LK <propixlk@gmail.com>
2025-09-06 13:09:13 +0530

    Verify system

---
+++ data-encoded.js
@@ -0,0 +1,6 @@
+// This file contains the student data in an encoded format (Base64).
+// This makes it harder for users to view or modify the data directly.
+// To update this data, you must re-encode your original data.js file.
+// See the README.md for instructions.
+
+const encodedStudentData = "WwogIHsgCiAgICAicmVnTnVtYmVyIjogIkFTUC8yMDIvMDAxIiwgCiAgICAibmFtZSI6ICJBLkIuUy5XaXRoYW5hZ2UiLCAKICAgICJjZXJ0aWZpY2F0ZUlkIjogImZhczAwMSIgCiAgfSwKICB7IAogICAgInJlZ051bWJlciI6ICJBU1AvMjAyLzAwMiIsIAogICAgIm5hbWUiOiAiU3JpcGFsYSIsIAogICAgImNlcnRpZmljYXRlSWQiOiAiZmFzMDAyIiAKICB9LAogIHsgCiAgICAicmVnTnVtYmVyIjogIkFTUC8yMDIvMDAzIiwgCiAgICAibmFtZSI6ICJBLkIuQy4gTmFtYSIsIAogICAgImNlcnRpZmljYXRlSWQiOiAiZmFzMDAzIiAKICB9Cl0=";
