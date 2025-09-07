ef85c7206a66cffa22def6ace0bbb8d356b5e73f
Inspira2025 <94883652+inspira2025@users.noreply.github.com>
2025-09-07 09:35:22 +0530

    0

---
+++ data.js
@@ -0,0 +1,17 @@
+const studentData = [
+  { 
+    "regNumber": "ASP/202/001", 
+    "name": "A.B.S.Withanage", 
+    "certificateId": "fas001" 
+  },
+  { 
+    "regNumber": "ASP/202/002", 
+    "name": "Sripala", 
+    "certificateId": "fas002" 
+  },
+  { 
+    "regNumber": "ASP/202/003", 
+    "name": "A.B.C. Nama", 
+    "certificateId": "fas004" 
+  },
+];
